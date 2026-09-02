1a8491e39102ebd58559117e4766c5c849101ce4
kimura0622 <kimura0622@gmail.com>
2026-09-02 21:25:12 +0800

    Add files via upload
    station-assistant-mobile-github-v5.8.3

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'station-assistant-mobile-github-v5.8.2';
+const CACHE_NAME = 'station-assistant-mobile-github-v5.8.3';
 const APP_SHELL = [
   './',
   './index.html',
   './manifest.webmanifest',
-  './vendor/pdf.min.js?v=5.8.2',
-  './vendor/pdf.worker.min.js?v=5.8.2',
-  './vendor/xlsx.full.min.js?v=5.8.2',
+  './vendor/pdf.min.js?v=5.8.3',
+  './vendor/pdf.worker.min.js?v=5.8.3',
+  './vendor/xlsx.full.min.js?v=5.8.3',
   './icons/icon-180.png',
   './icons/icon-192.png',
   './icons/icon-512.png'
